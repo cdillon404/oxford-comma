9de1fdd7488857799385694d1a8a41b75e74f7ec
cdillon404 <chavin@gmail.com>
2020-11-21 09:41:27 -0700

    commit tree 07c8a88e7964fd31a81973a54d1574c69248a3fd

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,23 @@
-def oxford_comma(array)
-
+=begin
+def oxford_comma(fruits)
+    return fruits.join(' and ') if fruits.size < 2
+    return fruits.join(' and') if fruits.size < 3
+    fruits[-1] = "and " + fruits[-1]
+    fruits.join(', ')
 end
+=end
+
+require 'pry'
+
+def oxford_comma(array)
+    if array.length == 2
+    array[-2] << " and "
+    array.join
+    elsif array.length == 1
+        array.join
+    elsif array.length > 2
+      array[-1].prepend "and "
+      array.join(", ")
+    end
+  end
+  
